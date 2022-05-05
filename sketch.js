3407c99006152a4593ad8c30b0ad7beede383752
AditiMarwah234 <104973840+AditiMarwah234@users.noreply.github.com>
2022-05-05 18:39:45 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+createCanvas(200, 200)
+ if (keyDown(RIGHT_ARROW)){
+canvasColor = "blue"   
+ }
+
+ if (keyDown(LEFT_ARROW)){
+   canvasColor = "purple"
+ }
+
+ if (keyDown(UP_ARROW)) {
+   canvasColor = "orange"
+ }
+
+ if (keyDown(UP_ARROW)) {
+   canvasColor = "pink"
+ }
+
